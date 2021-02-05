930cc58e60500153c227f468da60d50957fe8849
oscarjsv <gumball_junior@hotmail.com>
2021-02-04 21:12:08 -0500

    2-recurse.py

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,27 +1,24 @@
 #!/usr/bin/python3
-'''Get ALL hot posts'''
+"""Top Ten"""
 import requests
 
-BASE_URL = 'http://reddit.com/r/{}/hot.json'
 
+def recurse(subreddit, hot_list=[], after=""):
+    """
+    prints the titles of the first 10 hot
+    posts listed for a given subreddit
+    """
+    url = 'https://www.reddit.com/r/{}/hot.json?after={}'.format(
+        subreddit, after)
+    user_agent = {'User-Agent': 'jesgogu27'}
+    req = requests.get(url, headers=user_agent).json()
 
-def recurse(subreddit, hot_list=[], after=None):
-    '''Get ALL hot posts'''
-    headers = {'User-agent': 'Angel'}
-    params = {'limit': 100}
-    if isinstance(after, str):
-        if after != "STOP":
-            params['after'] = after
-        else:
-            return hot_list
-    response = requests.get(BASE_URL.format(subreddit),
-                            headers=headers, params=params)
-    if response.status_code != 200:
-        return None
-    data = response.json().get('data', {})
-    after = data.get('after', 'STOP')
-    if not after:
-        after = "STOP"
-    hot_list = hot_list + [post.get('data', {}).get('title')
-                           for post in data.get('children', [])]
-    return recurse(subreddit, hot_list, after)
+    if req.get('error') == 404:
+        return (None)
+    my_list = req.get('data').get('children')
+    for num in my_list:
+        hot_list.append(num.get('data').get('title'))
+    after = req.get('data').get('after')
+    if after:
+        return recurse(subreddit, hot_list, after)
+    return hot_list
